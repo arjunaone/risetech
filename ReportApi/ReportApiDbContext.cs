dd0f5d6f3e33a55bcfbbee2b569c1e2e8e80b1b4
arjunaone <onur.dokmetas@gmail.com>
2021-03-02 19:52:26 +0300

    b

---
+++ ReportApi/ReportApiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportApi
+{
+    public class ReportApiDbContext : DbContext
+    {
+        public ReportApiDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            ob.UseNpgsql("User ID=postgres;Password=;Host=localhost;Port=5432;Database=postgres;Pooling=true;", x => x.MigrationsAssembly("ReportApi"));
+            base.OnConfiguring(ob);
+        }
+
+        public DbSet<Report> Reports { get; set; }
+    }
+}
